2f02e46b0b2c488c637d965af08d0b4adf93afc5
JhonTMich <jhontmichael@gmail.com>
2026-05-03 12:28:19 -0500

    feat: configuracion de lambda.tf

--- iac/lambda.tf
+++ iac/lambda.tf
@@ -1,48 +1,151 @@
-# IAM role for Lambda execution
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
+data "archive_file" "upload_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/../src/upload-lambda"
+  output_path = "${path.module}/upload-lambda.zip"
+}
 
+data "archive_file" "crop_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/../src/crop-lambda"
+  output_path = "${path.module}/crop-lambda.zip"
+}
+
+resource "aws_cloudwatch_log_group" "upload_log" {
+  name              = "/aws/lambda/upload-lambda-${terraform.workspace}"
+  retention_in_days = 14
+}
+
+resource "aws_cloudwatch_log_group" "crop_log" {
+  name              = "/aws/lambda/crop-lambda-${terraform.workspace}"
+  retention_in_days = 14
+}
+
+data "aws_iam_policy_document" "lambda_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
     principals {
       type        = "Service"
       identifiers = ["lambda.amazonaws.com"]
     }
-
-    actions = ["sts:AssumeRole"]
   }
 }
 
-resource "aws_iam_role" "example" {
-  name               = "lambda_execution_role"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+resource "aws_iam_role" "upload_role" {
+  name               = "upload-lambda-role-${terraform.workspace}"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
 }
 
-# Package the Lambda function code
-data "archive_file" "example" {
-  type        = "zip"
-  source_file = "../src/lambda/index.js"
-  output_path = "${path.module}/lambda/function.zip"
+resource "aws_iam_role_policy_attachment" "upload_vpc_access" {
+  role       = aws_iam_role.upload_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
 }
 
-# Lambda function
-resource "aws_lambda_function" "example" {
-  filename      = data.archive_file.example.output_path
-  function_name = "example_lambda_function"
-  role          = aws_iam_role.example.arn
-  handler       = "index.handler"
-  code_sha256   = data.archive_file.example.output_base64sha256
+resource "aws_iam_role_policy" "upload_s3_policy" {
+  name = "s3-upload-policy"
+  role = aws_iam_role.upload_role.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect   = "Allow"
+      Action   = ["s3:PutObject"]
+      Resource = "${aws_s3_bucket.images_bucket.arn}/uploads/*"
+    }]
+  })
+}
 
-  runtime = "nodejs20.x"
+resource "aws_iam_role" "crop_role" {
+  name               = "crop-lambda-role-${terraform.workspace}"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "crop_vpc_access" {
+  role       = aws_iam_role.crop_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+}
+
+resource "aws_iam_role_policy" "crop_strict_policy" {
+  name = "crop-strict-policy"
+  role = aws_iam_role.crop_role.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = ["s3:GetObject"]
+        Resource = "${aws_s3_bucket.images_bucket.arn}/uploads/*"
+      },
+      {
+        Effect   = "Allow"
+        Action   = ["s3:PutObject"]
+        Resource = "${aws_s3_bucket.images_bucket.arn}/processed/*"
+      },
+      {
+        Effect   = "Allow"
+        Action   = [
+          "sqs:ReceiveMessage",
+          "sqs:DeleteMessage",
+          "sqs:GetQueueAttributes",
+          "sqs:ChangeMessageVisibility"
+        ]
+        Resource = aws_sqs_queue.main_queue.arn
+      }
+    ]
+  })
+}
+
+resource "aws_lambda_function" "upload_lambda" {
+  function_name    = "upload-lambda-${terraform.workspace}"
+  filename         = data.archive_file.upload_zip.output_path
+  source_code_hash = data.archive_file.upload_zip.output_base64sha256
+  role             = aws_iam_role.upload_role.arn
+  handler          = "index.handler"
+  runtime          = "nodejs20.x"
+  memory_size      = 256
+  timeout          = 30
+
+  vpc_config {
+    subnet_ids         = [aws_subnet.private_a.id, aws_subnet.private_b.id]
+    security_group_ids = [aws_security_group.sg_upload_lambda.id]
+  }
 
   environment {
     variables = {
-      ENVIRONMENT = "production"
-      LOG_LEVEL   = "info"
+      S3_BUCKET     = aws_s3_bucket.images_bucket.id
+      UPLOAD_PREFIX = "uploads/"
     }
   }
 
-  tags = {
-    Environment = "production"
-    Application = "example"
+  depends_on = [aws_cloudwatch_log_group.upload_log]
+}
+
+resource "aws_lambda_function" "crop_lambda" {
+  function_name    = "crop-lambda-${terraform.workspace}"
+  filename         = data.archive_file.crop_zip.output_path
+  source_code_hash = data.archive_file.crop_zip.output_base64sha256
+  role             = aws_iam_role.crop_role.arn
+  handler          = "index.handler"
+  runtime          = "nodejs20.x"
+  memory_size      = 512
+  timeout          = 60
+
+  vpc_config {
+    subnet_ids         = [aws_subnet.private_a.id, aws_subnet.private_b.id]
+    security_group_ids = [aws_security_group.sg_crop_lambda.id]
   }
+
+  environment {
+    variables = {
+      S3_BUCKET        = aws_s3_bucket.images_bucket.id
+      PROCESSED_PREFIX = "processed/"
+    }
+  }
+
+  depends_on = [aws_cloudwatch_log_group.crop_log]
+}
+
+resource "aws_lambda_event_source_mapping" "sqs_trigger" {
+  event_source_arn                   = aws_sqs_queue.main_queue.arn
+  function_name                      = aws_lambda_function.crop_lambda.arn
+  batch_size                         = 5
+  function_response_types            = ["ReportBatchItemFailures"]
 }
